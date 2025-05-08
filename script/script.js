0aedde9f37489610315acb200a66994282b811ea
Kevin Camargo <75258167+KevinARCamargo@users.noreply.github.com>
2025-05-08 20:57:04 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -42,3 +42,28 @@ const depoimentosSwiper = new Swiper('.depoimentosSwiper', {
         }
     }
 });
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+const menuItems = document.querySelectorAll('.nav-links a');
+
+// Abrir/Fechar o menu ao clicar no ícone
+menuToggle.addEventListener('click', (event) => {
+    navLinks.classList.toggle('show');
+    event.stopPropagation(); // Previne que o clique no botão feche o menu
+});
+
+// Fechar o menu ao clicar em qualquer link
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('show');
+    });
+});
+
+// Fechar o menu ao clicar fora dele
+document.addEventListener('click', (event) => {
+    const isClickInsideMenu = navLinks.contains(event.target) || menuToggle.contains(event.target);
+    if (!isClickInsideMenu) {
+        navLinks.classList.remove('show');
+    }
+});
